8a1aa55ca0a77154237a19bd976e33ed3c9e991c
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-03-20 22:29:36 +0200

    added make and model drop down lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
