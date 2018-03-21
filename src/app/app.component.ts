0e59ab5f0c5eaf86e0b4adc5f0b117f5e57928ff
Greta Piliponytė <g.piliponyte97@gmail.com>
2018-03-21 16:06:57 +0200

    finished linking with vehicle backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VehicleService } from './services/vehicle.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent{
   title = 'Lease Calculator';
 }
