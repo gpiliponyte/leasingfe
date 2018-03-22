e0cde4d1fd884f6924ddee36de6b9befb3ce9a71
gaijinn <grigoniskarolis@gmail.com>
2018-03-22 20:51:29 +0200

    added calculations and extra validation

--- src/app/leasing-form/leasing-form.component.ts
+++ src/app/leasing-form/leasing-form.component.ts
@@ -15,7 +15,7 @@ export class LeasingFormComponent implements OnInit {
   assetTypes = ['New', 'Old'];
   paymentDates = [15, 30];
   years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
-  numberRegex = '^\\s*(?=.*[1-9])\\d*(?:\\,\\d{1,2})?\\s*$';
+  numberRegex = '^\\s*(?=.*[1-9])\\d*(?:\\.\\d{1,2})?\\s*$';
 
   customerType;
   assetType;
@@ -40,6 +40,7 @@ export class LeasingFormComponent implements OnInit {
   showOK = false;
   changeScrollValue = true;
   selectedYear = '';
+  value;
 
   listVehicle;
 
@@ -66,26 +67,30 @@ export class LeasingFormComponent implements OnInit {
       'brand': new FormControl(null, Validators.required),
       'model': new FormControl(null, Validators.required),
       'year': new FormControl(null, Validators.required),
-      'enginePower': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex)]),
-      'assetPrice': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex)]),
-      'advancePaymentPercentage': new FormControl(null,
-        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(4)]),
-      'advancePaymentAmount': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex)]),
+      'enginePower': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex),
+        Validators.min(1)]),
+      'assetPrice': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex),
+        Validators.min(5000)]),
+      'advancePaymentPercentage': new FormControl(10,
+        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(5),
+          Validators.min(10)]),
+      'advancePaymentAmount': new FormControl(null),
       'leasePeriod': new FormControl(6, Validators.required),
-      'margin': new FormControl(null,
-        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(4)]),
+      'margin': new FormControl(3.2,
+        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(5),
+          Validators.min(3.2)]),
       'contractFee': new FormControl(200, Validators.required),
       'paymentDate': new FormControl(null, Validators.required)
     });
     this.loadVehicles();
-
+    this.onChanges();
   }
 
   getCarModelsByBrand(brand) {
-    if(this.changeScrollValue){
-      this.selectedModel = "";
+    if (this.changeScrollValue) {
+      this.selectedModel = '';
     }
-    this.changeScrollValue ? this.selectedModel = "" : this.changeScrollValue = true;
+    this.changeScrollValue ? this.selectedModel = '' : this.changeScrollValue = true;
     this.modelsBySelectedBrand = [];
     this.listVehicle.forEach(data => {
       console.log(data['groupValue']);
@@ -95,26 +100,73 @@ export class LeasingFormComponent implements OnInit {
     });
   }
 
-  goBackToForm(){
+  goBackToForm() {
     this.showFormSummary = false;
     this.showForm = true;
   }
 
-  submit(){
+  submit() {
     this.showFormSummary = false;
     this.showOK = true;
   }
 
   onSubmit() {
     console.log(this.leaseForm);
-     this.showFormSummary = true;
-     this.showForm = false;
-     this.changeScrollValue = false;
+    this.showFormSummary = true;
+    this.showForm = false;
+    this.changeScrollValue = false;
   }
 
   pitch(event: any) {
-  this.rangeValue=event.value;
+    this.rangeValue = event.value;
 
-}
+  }
+
+  onChanges() {
+    this.leaseForm.get('advancePaymentPercentage').valueChanges.subscribe(val => {
+      console.log(val);
+      if (this.leaseForm.get('advancePaymentPercentage').valid) {
+        this.value = this.leaseForm.get('assetPrice').value * (parseFloat(val) / 100);
+        console.log(parseFloat(val));
+        console.log(this.leaseForm.get('assetPrice').value);
+        console.log(this.value);
+        this.leaseForm.get('advancePaymentAmount').patchValue(this.value);
+      } else {
+        this.leaseForm.get('advancePaymentAmount').patchValue('');
+      }
+    });
+    this.leaseForm.get('assetPrice').valueChanges.subscribe(val => {
+      console.log('assetprice ' + val);
+      this.value = val * (1 / 100);
+      if (this.value > this.leaseForm.get('contractFee').value && this.value > 200) {
+        this.leaseForm.get('contractFee').patchValue(this.value);
+      }
+      if (this.value > 200) {
+        this.leaseForm.get('contractFee').patchValue(this.value);
+      } else {
+        this.leaseForm.get('contractFee').patchValue(200);
+      }
+      if (this.leaseForm.get('assetPrice').valid) {
+        this.leaseForm.get('advancePaymentAmount').patchValue(val * (this.leaseForm.get('advancePaymentPercentage').value / 100));
+      } else {
+        console.log('I am nan');
+        this.leaseForm.get('advancePaymentAmount').patchValue('');
+      }
+      console.log(parseFloat(val));
+      console.log(this.value);
+    });
+  }
+
+  test() {
+    // this.value = this.leaseForm.get('assetPrice').value * (parseFloat(this.leaseForm.get('advancePaymentAMount').value) * 100);
+    // console.log(this.leaseForm.get('assetPrice').value);
+    // console.log(this.value);
+    // this.leaseForm.get('advancePaymentPercentage').patchValue(this.value);
+    if (this.advancePaymentPercentage < 10 && this.advancePaymentPercentage > 100) {
+      this.advancePaymentAmount = this.assetPrice * (10 / 100);
+    } else {
+      this.advancePaymentAmount = this.assetPrice * (this.advancePaymentPercentage / 100);
+    }
+  }
 
 }
