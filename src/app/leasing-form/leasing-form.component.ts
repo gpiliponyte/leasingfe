9648bb2ba6469ca06e5d92c71bc64b3c7427cc7b
gaijinn <grigoniskarolis@gmail.com>
2018-03-23 11:56:39 +0200

    more validation

--- src/app/leasing-form/leasing-form.component.ts
+++ src/app/leasing-form/leasing-form.component.ts
@@ -12,10 +12,11 @@ export class LeasingFormComponent implements OnInit {
 
   defaultContractFee = 200;
   customerTypes = ['Private', 'Business'];
-  assetTypes = ['New', 'Old'];
+  assetTypes = ['New', 'Used'];
   paymentDates = [15, 30];
   years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018];
   numberRegex = '^\\s*(?=.*[1-9])\\d*(?:\\.\\d{1,2})?\\s*$';
+  validationRegex = '^([1-9])\\d*(?:\\.\\d{1,2})?\\s*$';
 
   customerType;
   assetType;
@@ -67,17 +68,17 @@ export class LeasingFormComponent implements OnInit {
       'brand': new FormControl(null, Validators.required),
       'model': new FormControl(null, Validators.required),
       'year': new FormControl(null, Validators.required),
-      'enginePower': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex),
+      'enginePower': new FormControl(null, [Validators.required, Validators.pattern(this.validationRegex),
         Validators.min(1)]),
-      'assetPrice': new FormControl(null, [Validators.required, Validators.pattern(this.numberRegex),
+      'assetPrice': new FormControl(null, [Validators.required, Validators.pattern(this.validationRegex),
         Validators.min(5000)]),
       'advancePaymentPercentage': new FormControl(10,
-        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(5),
+        [Validators.required, Validators.pattern(this.validationRegex), Validators.max(100),
           Validators.min(10)]),
       'advancePaymentAmount': new FormControl(null),
       'leasePeriod': new FormControl(6, Validators.required),
       'margin': new FormControl(3.2,
-        [Validators.required, Validators.pattern(this.numberRegex), Validators.max(100), Validators.maxLength(5),
+        [Validators.required, Validators.pattern(this.validationRegex), Validators.max(100),
           Validators.min(3.2)]),
       'contractFee': new FormControl(200, Validators.required),
       'paymentDate': new FormControl(null, Validators.required)
@@ -127,33 +128,40 @@ export class LeasingFormComponent implements OnInit {
       console.log(val);
       if (this.leaseForm.get('advancePaymentPercentage').valid) {
         this.value = this.leaseForm.get('assetPrice').value * (parseFloat(val) / 100);
-        console.log(parseFloat(val));
+        console.log('parsed value :' + parseFloat(val));
         console.log(this.leaseForm.get('assetPrice').value);
         console.log(this.value);
-        this.leaseForm.get('advancePaymentAmount').patchValue(this.value);
+        this.leaseForm.get('advancePaymentAmount').patchValue(this.value.toFixed(2));
       } else {
         this.leaseForm.get('advancePaymentAmount').patchValue('');
       }
     });
     this.leaseForm.get('assetPrice').valueChanges.subscribe(val => {
       console.log('assetprice ' + val);
-      this.value = val * (1 / 100);
-      if (this.value > this.leaseForm.get('contractFee').value && this.value > 200) {
-        this.leaseForm.get('contractFee').patchValue(this.value);
-      }
-      if (this.value > 200) {
-        this.leaseForm.get('contractFee').patchValue(this.value);
+      if (this.leaseForm.get('assetPrice').valid) {
+        this.value = val * (1 / 100);
+        if (this.value > this.leaseForm.get('contractFee').value && this.value > 200) {
+          this.leaseForm.get('contractFee').patchValue(this.value.toFixed(2));
+        }
+        if (this.value > 200) {
+          this.leaseForm.get('contractFee').patchValue(this.value.toFixed(2));
+        } else {
+          this.leaseForm.get('contractFee').patchValue(200);
+        }
+        if (this.leaseForm.get('assetPrice').valid) {
+          this.leaseForm.get('advancePaymentAmount').patchValue((
+            val * (this.leaseForm.get('advancePaymentPercentage').value / 100))
+            .toFixed(2));
+        } else {
+          console.log('I am nan');
+          this.leaseForm.get('advancePaymentAmount').patchValue('');
+        }
+        console.log(parseFloat(val));
+        console.log(this.value);
       } else {
         this.leaseForm.get('contractFee').patchValue(200);
-      }
-      if (this.leaseForm.get('assetPrice').valid) {
-        this.leaseForm.get('advancePaymentAmount').patchValue(val * (this.leaseForm.get('advancePaymentPercentage').value / 100));
-      } else {
-        console.log('I am nan');
         this.leaseForm.get('advancePaymentAmount').patchValue('');
       }
-      console.log(parseFloat(val));
-      console.log(this.value);
     });
   }
 
