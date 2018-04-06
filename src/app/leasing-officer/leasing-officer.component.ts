3613a88e7f64cb9752fdbfe895a37b311d28be9f
gaijinn <gaijinn@users.noreply.github.com>
2018-04-06 10:44:13 +0300

    front-end session expiration fix

--- src/app/leasing-officer/leasing-officer.component.ts
+++ src/app/leasing-officer/leasing-officer.component.ts
@@ -1,6 +1,7 @@
-
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LeaseService} from '../services/lease.service';
+import {TokenStorage} from '../core/token.storage';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-leasing-officer',
@@ -11,30 +12,38 @@ export class LeasingOfficerComponent implements OnInit {
 
   listOfLeases;
 
-  constructor(private leaseService: LeaseService) { }
+  constructor(private leaseService: LeaseService, private token: TokenStorage, private router: Router) {
+  }
 
   ngOnInit() {
 
   }
 
+  logout(): void {
+    this.token.signOut();
+    this.router.navigate(['login']);
+  }
+
   pendingLeases() {
     this.leaseService.getAllPendingLeases()
       .then(data => {
-        console.log('subscribe')
+        console.log('subscribe');
         this.listOfLeases = data;
       });
   }
+
   approvedLeases() {
     this.leaseService.getAllApprovedLeases()
       .then(data => {
-        console.log('subscribe')
+        console.log('subscribe');
         this.listOfLeases = data;
       });
   }
+
   deniedLeases() {
     this.leaseService.getAllDeniedLeases()
       .then(data => {
-        console.log('subscribe')
+        console.log('subscribe');
         this.listOfLeases = data;
       });
   }
