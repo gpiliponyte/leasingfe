61ed1b377cea07b9e0022869941fd394e7b07d1f
gaijinn <grigoniskarolis@gmail.com>
2018-04-06 13:52:40 +0300

    login error message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from '../core/auth.service';
 import {TokenStorage} from '../core/token.storage';
@@ -12,9 +12,13 @@ export class LoginComponent implements OnInit {
 
   username: string;
   password: string;
-  constructor(private router: Router, private authService: AuthService, private token: TokenStorage) { }
+  errorMessage: boolean;
+
+  constructor(private router: Router, private authService: AuthService, private token: TokenStorage) {
+  }
 
   ngOnInit() {
+    this.errorMessage = false;
   }
 
   login(): void {
@@ -22,9 +26,10 @@ export class LoginComponent implements OnInit {
       data => {
         this.token.saveToken(data.token);
         this.router.navigate(['leasingOfficer']);
+      }, error2 => {
+        this.errorMessage = true;
       }
     );
-    console.log('hello');
   }
 
 }
